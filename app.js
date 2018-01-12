f6786b70e248594ec5029357391209eb9a82ac6e
Hardik Chheda <Hardik Chheda>
2018-01-12 15:42:35 -0500

    Adding app.js and package.json

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const configRoutes = require("./routes");
+const exphbs  = require('express-handlebars');
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+const static = express.static(__dirname + '/public');
+app.use("/public", static);
+app.use(bodyParser.json());
+var hbs = require('handlebars');
+hbs.registerHelper("concat", function(status)
+{
+    //console.log(status)
+    return "localhost:3000/restaurants/"+status
+});
+configRoutes(app);
+
+app.listen(3000, () => {
+    console.log("We've now got a server!");
+    console.log("Your routes will be running on http://localhost:3000");
+});
