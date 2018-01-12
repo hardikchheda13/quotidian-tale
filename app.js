751cf556dd8df303a241b26c435df11268c2176f
Hardik Chheda <Hardik Chheda>
2018-01-12 17:50:46 -0500

    new commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const configRoutes = require("./routes");
+//const configRoutes = require("./routes");
 const exphbs  = require('express-handlebars');
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
@@ -11,8 +11,7 @@ app.use(bodyParser.json());
 var hbs = require('handlebars');
 //hardik chheddhahaa
 //hardik chheda new changes
-//hardik new changes done jasmeet bhenchod
-configRoutes(app);
+//configRoutes(app);
 //Jasmeet New changes done 
 app.listen(3000, () => {
     console.log("We've now got a server!");
